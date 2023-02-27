702c5f89faa5087b99f1bd720d2e0aff2c6a9a51
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-02-27 08:50:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ if(process.env.NODE_ENV ==='production')
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log("Server running on port 🔥"));
+app.listen(port, () => console.log("Server running on"+port));
